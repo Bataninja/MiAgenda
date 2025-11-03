c4cbcb92042b9e1b0ad84ac72a473c4aa4b2f41e
Frank <frankcamilogordillopea@gmail.com>
2025-11-02 20:41:36 -0500

    Se añaden mas elementos al crud para empezar a realizar las pruebas con postman

--- src/routes/eventRoutes.js
+++ src/routes/eventRoutes.js
@@ -1,11 +1,25 @@
 const express = require("express");
-const router = express.Router(); 
+const router = express.Router();
 const eventSchema = require("../models/eventModel");
 
-router.post("/events", (req, res) => {
+router.post("/saveEvent", (req, res) => {
     const event = eventSchema(req.body);
     event
         .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/consultEvents", (req, res) => {
+    eventSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/consultEvents/id", (req, res) => {
+    const { id } = req.params;
+    eventSchema
+        .findById(id)
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
